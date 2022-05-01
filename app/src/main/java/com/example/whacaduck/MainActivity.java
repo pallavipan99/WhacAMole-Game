f37217959e43c43dea985a794d92e88dd08df1fd
Pallavi <96259165+pallavipan99@users.noreply.github.com>
2022-05-01 10:30:00 -0400

    Implemented mole spawning logic

--- app/src/main/java/com/example/whacaduck/MainActivity.java
+++ app/src/main/java/com/example/whacaduck/MainActivity.java
@@ -140,6 +140,163 @@ public class MainActivity extends AppCompatActivity {
         images.clone(constraintLayout);
 
 
+/*
+
+        ImageView click = new ImageView(this);
+        click.setId(View.generateViewId());
+        click.setImageResource(R.drawable.countduck);
+
+        ConstraintLayout.LayoutParams param = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+
+        click.setLayoutParams(param);
+        constraintLayout2.addView(click);
+
+        ConstraintSet images2 = new ConstraintSet();
+        images2.clone(constraintLayout2);
+        images2.connect(clickCount.getId(), ConstraintSet.TOP, constraintLayout2.getId(), ConstraintSet.TOP);
+        images2.connect(clickCount.getId(), ConstraintSet.BOTTOM, constraintLayout2.getId(), ConstraintSet.BOTTOM);
+        images2.connect(clickCount.getId(), ConstraintSet.LEFT, constraintLayout2.getId(), ConstraintSet.LEFT);
+        images2.connect(clickCount.getId(), ConstraintSet.RIGHT, constraintLayout2.getId(), ConstraintSet.RIGHT);
+
+        images2.setHorizontalBias(clickCount.getId(), .445f);
+        images2.setVerticalBias(clickCount.getId(), 0f);
+
+        images2.applyTo(constraintLayout2);
+
+         */
+
+
+        duck.setVisibility(View.INVISIBLE);
+        duck2.setVisibility(View.INVISIBLE);
+        duck3.setVisibility(View.INVISIBLE);
+        duck4.setVisibility(View.INVISIBLE);
+        duck5.setVisibility(View.INVISIBLE);
+        duck6.setVisibility(View.INVISIBLE);
+        duck7.setVisibility(View.INVISIBLE);
+        duck8.setVisibility(View.INVISIBLE);
+        duck9.setVisibility(View.INVISIBLE);
+
+        Animation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeIn.setDuration(8000);
+
+        Animation fadeOut = new AlphaAnimation(1.0f, 0.0f);
+        fadeOut.setStartOffset(1000);
+        fadeOut.setDuration(8000);
+
+        AnimationSet animation = new AnimationSet(true);
+
+        Timer timer1 = new Timer();
+        TimerTask time = new TimerTask() {
+            @Override
+            public void run() {
+                randNum2 = (int) (Math.random() * 9);
+                randomNum = (int) (Math.random() * 10);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (randNum2 == 1) {
+                            if (randomNum == 1) {
+                                duck.setImageResource(R.drawable.bonusduck);
+                                duck.setTag(R.drawable.bonusduck);
+                                resource = R.drawable.bonusduck;
+                            }
+                            else{
+                                resource = 0;
+                            }
+                            duck.setVisibility(View.VISIBLE);
+
+                        }
+                        if (randNum2 == 2) {
+                            if (randomNum == 1) {
+                                duck2.setImageResource(R.drawable.bonusduck);
+                                duck2.setTag(R.drawable.bonusduck);
+                                resource2 = R.drawable.bonusduck;
+                            }
+                            else{
+                                resource2 = 0;
+                            }
+                            duck2.setVisibility(View.VISIBLE);
+                        }
+                        if (randNum2 == 3) {
+                            if (randomNum == 1) {
+                                duck3.setImageResource(R.drawable.bonusduck);
+                                duck3.setTag(R.drawable.bonusduck);
+                                resource3 = R.drawable.bonusduck;
+                            }
+                            else{
+                                resource3 =0;
+
+                            }
+                            duck3.setVisibility(View.VISIBLE);
+                        }
+                        if (randNum2 == 4) {
+                            if (randomNum == 1) {
+                                duck4.setImageResource(R.drawable.bonusduck);
+                                duck4.setTag(R.drawable.bonusduck);
+                                resource4 = R.drawable.bonusduck;
+                            }
+                            else{
+                                resource4 = 0;
+                            }
+                            duck4.setVisibility(View.VISIBLE);
+                        }
+                        if (randNum2 == 5) {
+                            if (randomNum == 1) {
+                                duck5.setImageResource(R.drawable.bonusduck);
+                                duck5.setTag(R.drawable.bonusduck);
+                                resource5 = R.drawable.bonusduck;
+                            }
+                            duck5.setVisibility(View.VISIBLE);
+                            resource5 = 0;
+                        }
+                        if (randNum2 == 6) {
+                            if (randomNum == 1) {
+                                duck6.setImageResource(R.drawable.bonusduck);
+                                duck6.setTag(R.drawable.bonusduck);
+                                resource6 = R.drawable.bonusduck;
+                            }
+                            else{
+                                resource6 = 0;
+                            }
+                            duck6.setVisibility(View.VISIBLE);
+
+                        }
+                        if (randNum2 == 7) {
+                            if (randomNum == 1) {
+                                duck7.setImageResource(R.drawable.bonusduck);
+                                duck7.setTag(R.drawable.bonusduck);
+                                resource7 = R.drawable.bonusduck;
+                            }
+                            else{
+                                resource7 = 0;
+                            }
+                            duck7.setVisibility(View.VISIBLE);
+                        }
+                        if (randNum2 == 8) {
+                            if (randomNum == 1) {
+                                duck8.setImageResource(R.drawable.bonusduck);
+                                duck8.setTag(R.drawable.bonusduck);
+                                resource8 = R.drawable.bonusduck;
+                            }
+                            else{
+                                resource8 = 0;
+                            }
+                            duck8.setVisibility(View.VISIBLE);
+
+                        }
+                        if (randNum2 == 9) {
+                            if (randomNum == 1) {
+                                duck9.setImageResource(R.drawable.bonusduck);
+                                duck9.setTag(R.drawable.bonusduck);
+                                resource9 = R.drawable.bonusduck;
+                            }
+                            else{
+                                resource9 = 0;
+                            }
+                            duck9.setVisibility(View.VISIBLE);
+                        }
+
+
 
     }
 }
